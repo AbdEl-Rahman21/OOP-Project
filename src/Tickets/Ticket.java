d14a05b5b303d739f0e6247b3f1d47486bc94755
Lenovo <Lenovo@LEGION>
2023-12-07 16:21:43 +0200

    add Ticket class

--- src/Tickets/Ticket.java
+++ src/Tickets/Ticket.java
@@ -1,4 +1,20 @@
 package Tickets;
+import java.util.ArrayList;
+public abstract class Ticket {
+    private final String ticketID;
+    private final  String tripId;
+    private int num_of_pocket_seats;
+    private String Booked_Hotel;
+    private String Booked_Flight;
+    private String car_rental;
+    private ArrayList<String> activities=new ArrayList<>();
 
-public class Ticket {
+
+    public Ticket(String ticketID,String tripID, int num_of_pocket_seats ){
+        this.ticketID = ticketID;
+        this.tripId =tripID;
+        this.num_of_pocket_seats=num_of_pocket_seats;
+    }
+
 }
+

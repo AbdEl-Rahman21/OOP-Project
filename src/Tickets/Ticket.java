19b9df4614a538ce43b774b6cbba96537ed21427
Lenovo <Lenovo@LEGION>
2023-12-07 16:36:29 +0200

    add Ticket class getters setters

--- src/Tickets/Ticket.java
+++ src/Tickets/Ticket.java
@@ -16,5 +16,54 @@ public abstract class Ticket {
         this.num_of_pocket_seats=num_of_pocket_seats;
     }
 
+    public String getTripId() {
+        return tripId;
+
+    }
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public String getBooked_Flight() {
+        return Booked_Flight;
+    }
+
+    public String getBooked_Hotel() {
+        return Booked_Hotel;
+    }
+
+    public String getCar_rental() {
+        return car_rental;
+    }
+
+    public void setActivities(ArrayList<String> activities) {
+        this.activities = activities;
+    }
+
+    public void setBooked_Flight(String booked_Flight) {
+        Booked_Flight = booked_Flight;
+    }
+
+    public int getNum_of_pocket_seats() {
+        return num_of_pocket_seats;
+    }
+
+    public void setBooked_Hotel(String booked_Hotel) {
+        Booked_Hotel = booked_Hotel;
+    }
+
+    public void setCar_rental(String car_rental) {
+        this.car_rental = car_rental;
+    }
+
+    public void setNum_of_pocket_seats(int num_of_pocket_seats) {
+        this.num_of_pocket_seats = num_of_pocket_seats;
+    }
+
+    public ArrayList<String> getActivities() {
+        return activities;
+    }
+
 }
 
